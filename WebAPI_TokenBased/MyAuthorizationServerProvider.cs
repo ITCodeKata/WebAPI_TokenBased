cb3a0ff04bfa66c1fa27a393f721845976727220
Gaurav <gaurav.sharma@chilindo.com>
2017-11-24 14:03:26 +0700

    Add project files.

---
+++ WebAPI_TokenBased/MyAuthorizationServerProvider.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace WebAPI_TokenBased
+{
+    //Ref: https://www.youtube.com/watch?v=rMA69bVv0U8&t=289s
+    public class MyAuthorizationServerProvider:OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if(context.UserName =="admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Admin as: Gaurav Sharma"));
+                context.Validated(identity);
+            }
+            else if (context.UserName=="user" && context.Password=="user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "User as: Amit Sharma"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "provided username and password is incorrect");
+                return;
+            }
+        }
+    }
+}
